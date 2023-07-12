28475eac320278b3db5d27150747796c6130d3a2
KombinatorVit <slonik0775@gmail.com>
2023-07-12 10:51:05 +0300

    Game module refactoring part 1

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,52 +1,19 @@
-import { useState } from "react";
-import {
-  GameInfo,
-  GameTitle,
-  GameField,
-  useGameState,
-} from "../components/game";
 import { Header } from "../components/header";
-import { GameSymbol } from "../components/game/game-symbol";
+import { Game } from "../components/game-new";
 
 export default function HomePage() {
-  const [playersCount] = useState(4);
-  const {
-    cells,
-    currentMove,
-    handleCellClick,
-    nextMove,
-    winnerSequence,
-    handlePlayerTimeOver,
-    winnerSymbol,
-  } = useGameState(playersCount);
+  return (
+    <HomePageLayout header={<Header />}>
+      <Game />
+    </HomePageLayout>
+  );
+}
 
+function HomePageLayout({ header, children }) {
   return (
     <div className="bg-slate-50 min-h-screen">
-      <Header />
-      <main className="pt-6 mx-auto w-max">
-        <GameTitle playersCount={playersCount} />
-        <GameInfo
-          playersCount={playersCount}
-          className="mt-4 "
-          currentMove={currentMove}
-          isWinner={!!winnerSymbol}
-          onPlayerTimeOver={handlePlayerTimeOver}
-        />
-        {winnerSymbol && (
-          <div className="my-4">
-            <GameSymbol symbol={winnerSymbol} />
-          </div>
-        )}
-        <GameField
-          className="mt-6"
-          cells={cells}
-          currentMove={currentMove}
-          nextMove={nextMove}
-          handleCellClick={handleCellClick}
-          winnerSequence={winnerSequence}
-          winnerSymbol={winnerSymbol}
-        />
-      </main>
+      {header}
+      <main className="pt-6 mx-auto w-max">{children}</main>
     </div>
   );
 }
